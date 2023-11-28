0ef9dabc23b53e314801e23e3bc6aaf0cf80a6a8
pcstoyle <pcstoyle@hotmail.com>
2023-11-28 12:48:30 -0700

    updated js files addding and getting data from DB, installed event listeners

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,9 +4,6 @@ const path = require('path');
 const {InjectManifest} = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -23,10 +20,11 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      new MiniCssExtractPlugin();
+      new MiniCssExtractPlugin(),
+
       new InjectManifest ({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       })
 
     ],
